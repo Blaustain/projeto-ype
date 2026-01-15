ff220f995c3d05b53c1a23a3e67b0ef32649bea2
Blaustain <blaustainsouzaaraujo@gmail.com>
2026-01-15 20:48:30 -0300

    projeto ype

---
+++ script.js
@@ -0,0 +1,24 @@
+function acessarSistema() {
+    const matricula = document.getElementById('matricula-input').value;
+    if(matricula) {
+        document.getElementById('login-screen').classList.add('hidden');
+        document.getElementById('app-content').classList.remove('hidden');
+        document.getElementById('display-matricula').innerText = matricula;
+    } else {
+        alert("Por favor, informe a matrícula.");
+    }
+}
+
+function switchForm(cor) {
+    
+    document.querySelectorAll('.form-section').forEach(form => {
+        form.classList.remove('active-form');
+    });
+    
+    document.getElementById('form-' + cor).classList.add('active-form');
+}
+
+function enviarSAP(tipo) {
+    const mat = document.getElementById('display-matricula').innerText;
+    alert(`Enviando para SAP...\nEtiqueta: ${tipo}\nMatrícula: ${mat}\nStatus: Processando Ordem.`);
+}
